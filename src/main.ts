861525d7c3390209c3da71937f1a1b01b8a2e245
Kiran Randhawa <4026553+TheDevelolper@users.noreply.github.com>
2026-02-14 12:33:43 +0000

    chore(gh-pages): :rocket: Dist added for github pages
    Removes dist folder from gitignore for github pages (only for this branch).

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,12 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
+import { APP_BASE_HREF } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
-bootstrapApplication(App, appConfig)
+
+
+appConfig.providers.push({provide: APP_BASE_HREF, useValue: '/ng-guv-uk/'});
+
+  bootstrapApplication(App, appConfig)
   .catch((err) => console.error(err));
